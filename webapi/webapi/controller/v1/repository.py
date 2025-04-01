8a348d3eeb6f96279c2e01f10c475b53b27e1b48
Hossein Abdollahi <Hossein.abdn@gmail.com>
2025-04-01 21:30:35 +0000

    Add repository resource

---
+++ webapi/webapi/controller/v1/repository.py
@@ -0,0 +1,18 @@
+from webapi.util import jsonify
+
+
+class RepositoryController:
+    def get_repositories():
+        return jsonify(state=501, code=101)
+
+    def get_repository(repository_id):
+        return jsonify(state=501, code=101)
+
+    def create_repository():
+        return jsonify(state=501, code=101)
+
+    def update_repository(repository_id):
+        return jsonify(state=501, code=101)
+
+    def delete_repository(repository_id):
+        return jsonify(state=501, code=101)
